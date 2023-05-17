ab3c6f6711203a46ab7b778acecd191497652fd4
unknown <arabkhanramazanov@gmail.com>
2023-05-17 21:28:25 +0400

    Spring-boot introduction

---
+++ src/main/java/az/spring/demo/springbootdemo/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package az.spring.demo.springbootdemo.controller;
+
+import az.spring.demo.springbootdemo.dto.EmployeeDto;
+import az.spring.demo.springbootdemo.model.Employee;
+import az.spring.demo.springbootdemo.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+    @GetMapping("/employees")
+    public List<EmployeeDto>getEmployees(){
+        return employeeService.getEmployees();
+    }
+}
---
+++ src/main/java/az/spring/demo/springbootdemo/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package az.spring.demo.springbootdemo.service;
+
+import az.spring.demo.springbootdemo.dto.EmployeeDto;
+import az.spring.demo.springbootdemo.model.Employee;
+import az.spring.demo.springbootdemo.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public List<EmployeeDto>getEmployees(){
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream()
+                .map(employee -> getEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+
+    private EmployeeDto getEmployeeDto(Employee employee){
+        EmployeeDto employeeDto= new EmployeeDto();
+        BeanUtils.copyProperties(employee,employeeDto);
+        return employeeDto;
+    }
+
+}
